1f1cd28953b4ed06409e92832122d5b31e550bc1
mekk1t <kit.kalimov@gmail.com>
2021-03-16 21:26:57 +0300

    Тест

---
+++ TestCi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
