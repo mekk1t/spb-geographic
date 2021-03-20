57a0b3b9c22e383732907bbcaae9e1dc19ab362a
mekk1t <kit.kalimov@gmail.com>
2021-03-20 23:28:40 +0300

    Базовая модель упражнения
    Содержит ID для идентифицирования, поле правильного ответа и строку изображения

---
+++ Trainer/Models/Exercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SUAI.SpbGeographic.Trainer.Models
+{
+    public class Exercise
+    {
+        public Guid Id { get; }
+        public string CorrectAnswer { get; }
+        public string ImageBase64 { get; }
+
+        public Exercise(string correctAnswer, string imageBase64)
+        {
+            Id = Guid.NewGuid();
+            CorrectAnswer = correctAnswer;
+            ImageBase64 = imageBase64;
+        }
+    }
+}
