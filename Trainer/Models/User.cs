869b1baa0adda8ea0b42481c5adc7c09cb5f81c4
mekk1t <kit.kalimov@gmail.com>
2021-03-20 23:11:04 +0300

    Базовая модель пользователя

---
+++ Trainer/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SUAI.SpbGeographic.Trainer.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
