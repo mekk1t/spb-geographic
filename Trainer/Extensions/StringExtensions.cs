38f65d97787d980cd36680b5226e0e4d682ec905
mekk1t <kit.kalimov@gmail.com>
2021-03-20 23:22:29 +0300

    Расширение для хэширования пароля

---
+++ Trainer/Extensions/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Security.Cryptography;
+
+namespace SUAI.SpbGeographic.Trainer.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Получает хэш пароля, используя алгоритм PBKDF2. Реализация взята из документации Microsoft.
+        /// </summary>
+        public static string Hash(this string password)
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            string hash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return hash;
+        }
+    }
+}
--- Trainer/Models/User.cs
+++ Trainer/Models/User.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using SUAI.SpbGeographic.Trainer.Extensions;
+using System;
 
 namespace SUAI.SpbGeographic.Trainer.Models
 {
     public class User
     {
-        public Guid Id { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
+        public Guid Id { get; }
+        public string Username { get; }
+        public string PasswordHash { get; }
+
+        public User(string username, string password)
+        {
+            Id = Guid.NewGuid();
+            Username = username;
+            PasswordHash = password.Hash();
+        }
     }
 }
