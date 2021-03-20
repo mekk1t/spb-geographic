1d50838f3c036f2863d445d52a12cbed0c0c4ab9
mekk1t <kit.kalimov@gmail.com>
2021-03-20 23:32:11 +0300

    Базовая модель теста

---
+++ Trainer/Models/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SUAI.SpbGeographic.Trainer.Models
+{
+    public class Test
+    {
+        public Guid Id { get; }
+        public IEnumerable<Exercise> Exercises { get; }
+        public string Author { get; }
+
+        public Test(IEnumerable<Exercise> exercises, string author)
+        {
+            Exercises = exercises;
+            Author = author;
+        }
+    }
+}
