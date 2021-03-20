dd57e5d0087fc8a8907016bc9d56d0022e044030
mekk1t <kit.kalimov@gmail.com>
2021-03-21 00:09:20 +0300

    Перечисление для хранения уровней доступа пользователей в приложении

---
+++ Trainer/Models/AccessLevel.cs
@@ -0,0 +1,12 @@
+﻿namespace SUAI.SpbGeographic.Trainer.Models
+{
+    public enum AccessLevel
+    {
+        Banned = -1,
+        Default = 0,
+        CommonUser = 1,
+        Teacher = 2,
+        Administrator = 3,
+        Superadmin = 4
+    }
+}
