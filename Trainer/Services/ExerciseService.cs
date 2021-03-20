9890ca1f39cd31ba6ea43663e60f98c188c4e2eb
mekk1t <kit.kalimov@gmail.com>
2021-03-21 00:15:33 +0300

    Наброски сервиса по упражнениям

---
+++ Trainer/Services/ExerciseService.cs
@@ -0,0 +1,21 @@
+﻿using SUAI.SpbGeographic.Trainer.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SUAI.SpbGeographic.Trainer.Services
+{
+    public class ExerciseService
+    {
+        public IEnumerable<Exercise> GetAllExercises() => new List<Exercise>();
+
+        public IEnumerable<Exercise> GetExercisesFiltered(ExerciseFilter filter) => throw new NotImplementedException();
+
+        public Exercise GetExerciseById(Guid exerciseId) => throw new NotImplementedException();
+
+        public void CreateNewExercise(Exercise exercise) => throw new NotImplementedException();
+
+        public void EditExercise(Exercise exercise) => throw new NotImplementedException();
+
+        public void DeleteExerciseById(Guid exerciseId) => throw new NotImplementedException();
+    }
+}
