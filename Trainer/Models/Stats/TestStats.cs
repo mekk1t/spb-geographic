7fcb26beab5b5eb54621fa72c2b524190fa3ec7f
mekk1t <kit.kalimov@gmail.com>
2021-03-21 10:25:31 +0300

    Наполнение моделей статистики

--- Trainer/Models/Stats/TestStats.cs
+++ Trainer/Models/Stats/TestStats.cs
@@ -1,6 +1,10 @@
-﻿namespace SUAI.SpbGeographic.Trainer.Models.Stats
+﻿using System.Collections.Generic;
+
+namespace SUAI.SpbGeographic.Trainer.Models.Stats
 {
     public class TestStats
     {
+        public IEnumerable<UserDetails> UsersAttemptedThisTest { get; }
+        public IEnumerable<UserDetails> UsersPassedThisTest { get; }
     }
 }
--- Trainer/Models/Stats/UserStats.cs
+++ Trainer/Models/Stats/UserStats.cs
@@ -1,7 +1,14 @@
-﻿namespace SUAI.SpbGeographic.Trainer.Models
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SUAI.SpbGeographic.Trainer.Models
 {
     public class UserStats
     {
+        public Dictionary<Test, bool> TestsCondition { get; set; }
+        public Dictionary<Exercise, bool> ExercisesCondition { get; set; }
 
+        public int ExercisesCompleted => ExercisesCondition.Where(ec => ec.Value == true).Count();
+        public int TestsCompleted => TestsCondition.Where(tc => tc.Value == true).Count();
     }
 }
