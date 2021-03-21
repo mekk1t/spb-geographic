e29e2f3b44f4d1d1c86ed7c64bd0c8b306d40f6d
mekk1t <kit.kalimov@gmail.com>
2021-03-21 10:25:54 +0300

    Абстракция репозитория тестов

---
+++ Trainer/Abstractions/ITestRepository.cs
@@ -0,0 +1,15 @@
+﻿using SUAI.SpbGeographic.Trainer.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SUAI.SpbGeographic.Trainer.Abstractions
+{
+    public interface ITestRepository
+    {
+        IEnumerable<Test> GetAllTests();
+        IEnumerable<Test> GetFilteredTests(TestFilter filter);
+        void CreateNewTest(Test test);
+        void EditTest(Test test);
+        void DeleteTest(Guid testId);
+    }
+}
