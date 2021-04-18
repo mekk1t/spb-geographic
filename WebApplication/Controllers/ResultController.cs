87a3d4aec05028901e1fda5d52543fdc692016c0
mekk1t <kit.kalimov@gmail.com>
2021-04-18 07:37:49 +0300

    Скелет контроллера, который будет отдавать результат выполнения теста

---
+++ WebApplication/Controllers/ResultController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class ResultController : Controller
+    {
+        /// <summary>
+        /// Возвращает представление по завершении теста.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Complete() => View();
+
+        /// <summary>
+        /// Возвращает частичное представление с результатами на страницу теста, чтобы не перезагружать страницу.
+        /// </summary>
+        /// <remarks>
+        /// Win, FiftyFifty и Fail - названия .cshtml-файлов в папке Views/Result/.
+        /// </remarks>
+        /// <param name="result">Высчитанный % завершения теста.</param>
+        public IActionResult Stats(double result)
+        {
+            if (result >= 0.8)
+            {
+                return PartialView("Win");
+            }
+
+            if (result < 0.8 && result >= 0.5)
+            {
+                return PartialView("FiftyFifty");
+            }
+
+            return PartialView("Fail");
+        }
+    }
+}
