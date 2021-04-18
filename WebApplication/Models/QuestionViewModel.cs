5702d33ed655e09cb4084f1fb2651a75577ba72c
mekk1t <kit.kalimov@gmail.com>
2021-04-18 07:44:55 +0300

    Модель вопроса в тесте

---
+++ WebApplication/Models/QuestionViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication.Models
+{
+    public class QuestionViewModel
+    {
+        public string ImageBase64 { get; set; }
+        public string[] Options { get; set; }
+        public string CorrectAnswer { get; set; }
+    }
+}
