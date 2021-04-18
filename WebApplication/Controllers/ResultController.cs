a1bb855e9e07a53111bf073ba00a65d348e977f5
mekk1t <kit.kalimov@gmail.com>
2021-04-18 08:45:43 +0300

    Тестовый пример ТЕСТА

--- WebApplication/Controllers/ResultController.cs
+++ WebApplication/Controllers/ResultController.cs
@@ -1,35 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
     public class ResultController : Controller
     {
-        /// <summary>
-        /// Возвращает представление по завершении теста.
-        /// </summary>
-        /// <returns></returns>
-        public IActionResult Complete() => View();
-
-        /// <summary>
-        /// Возвращает частичное представление с результатами на страницу теста, чтобы не перезагружать страницу.
-        /// </summary>
-        /// <remarks>
-        /// Win, FiftyFifty и Fail - названия .cshtml-файлов в папке Views/Result/.
-        /// </remarks>
-        /// <param name="result">Высчитанный % завершения теста.</param>
-        public IActionResult Stats(double result)
+        public IActionResult Index(IEnumerable<QuestionViewModel> results)
         {
-            if (result >= 0.8)
+            var correctAnswersCount = results.Count(r => r.SelectedAnswer == r.CorrectAnswer);
+            var correctAnswersRatio = (double)correctAnswersCount / results.Count();
+
+            if (correctAnswersRatio >= 0.5)
             {
-                return PartialView("Win");
+                return View("Pass");
             }
 
-            if (result < 0.8 && result >= 0.5)
-            {
-                return PartialView("FiftyFifty");
-            }
-
-            return PartialView("Fail");
+            return View("Fail");
         }
     }
 }
--- WebApplication/Models/QuestionViewModel.cs
+++ WebApplication/Models/QuestionViewModel.cs
@@ -5,5 +5,6 @@
         public string ImageBase64 { get; set; }
         public string[] Options { get; set; }
         public string CorrectAnswer { get; set; }
+        public string SelectedAnswer { get; set; }
     }
 }
