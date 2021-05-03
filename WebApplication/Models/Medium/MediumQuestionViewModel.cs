2c65d2215f6b1a092e2c302e2f0541e275468c8a
mekk1t <kit.kalimov@gmail.com>
2021-05-03 16:53:31 +0300

    Создана модель вопроса для среднего теста

---
+++ WebApplication/Models/Medium/MediumQuestionViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication.Models.Medium
+{
+    public class MediumQuestionViewModel
+    {
+        /// <summary>
+        /// Главное изображение вопроса: обложка.
+        /// </summary>
+        public string Thumbnail { get; set; }
+        /// <summary>
+        /// Пользовательский ввод.
+        /// </summary>
+        public string UserInput { get; set; }
+        /// <summary>
+        /// Сам вопрос, например "В каком районе находится эта достопримечательность?" или "Как называется эта станция метро?" итд
+        /// </summary>
+        public string Question { get; set; }
+        /// <summary>
+        /// Набор правильных ответов.
+        /// </summary>
+        /// <remarks>
+        /// Здесь могут быть несколько вариантов правильного ответа: синонимы, разный регистр, название "в народе" итд.
+        /// </remarks>
+        public List<string> PossibleAnswers { get; set; }
+    }
+}
