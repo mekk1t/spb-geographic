d4fc736a7c29e0e02910ad6d477f76f86583cc08
mekk1t <kit.kalimov@gmail.com>
2021-05-03 16:53:18 +0300

    Удалена неиспользующаяся модель сложного теста

--- WebApplication/Models/Hard/HardTestViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace WebApplication.Models.Hard
-{
-    public class HardTestViewModel
-    {
-        public List<HardQuestionViewModel> Questions { get; set; }
-    }
-}
